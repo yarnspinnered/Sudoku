247de052d64e266bfc3a333dec02873b9b8354b2
j <redyarnspinner@gmail.com>
2018-01-13 20:08:13 +0800

    Cleaned up version

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -15,21 +15,21 @@ public class Sudoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int[][] boardArgs = new int[9][9];
+//       int[][] boardArgs = new int[9][9];
+//        
+//        boardArgs[0] = new int[]{5,3,0,0,0,0,0,0,0};
+//        boardArgs[1] = new int[]{6,0,0,1,9,5,0,0,0};
+//        boardArgs[2] = new int[]{0,9,8,0,0,0,0,6,0};
+//        boardArgs[3] = new int[]{8,0,0,0,6,0,0,0,3};
+//        boardArgs[4] = new int[]{4,0,0,8,0,3,0,0,1};
+//        boardArgs[5] = new int[]{7,0,0,0,2,0,0,0,6};
+//        boardArgs[6] = new int[]{0,6,0,0,0,0,2,8,0};
+//        boardArgs[7] = new int[]{0,0,0,4,1,9,0,0,5};
+//        boardArgs[8] = new int[]{0,0,0,0,8,0,0,7,9};
         
-        boardArgs[0] = new int[]{5,3,0,0,0,0,0,0,0};
-        boardArgs[1] = new int[]{6,0,0,1,9,5,0,0,0};
-        boardArgs[2] = new int[]{0,9,8,0,0,0,0,6,0};
-        boardArgs[3] = new int[]{8,0,0,0,6,0,0,0,3};
-        boardArgs[4] = new int[]{4,0,0,8,0,3,0,0,1};
-        boardArgs[5] = new int[]{7,0,0,0,2,0,0,0,6};
-        boardArgs[6] = new int[]{0,6,0,0,0,0,2,8,0};
-        boardArgs[7] = new int[]{0,0,0,4,1,9,0,0,5};
-        boardArgs[8] = new int[]{0,0,0,0,8,0,0,7,9};
-        
-        Board actualBoard = new Board(boardArgs);
-//        System.out.println(actualBoard.checkBoard());
-//        actualBoard.solveBoard(0);
+//        Board actualBoard = new Board(boardArgs);
+
+        Board actualBoard = Board.generateBoard();
         GUI x = new GUI(actualBoard);
         
     }
